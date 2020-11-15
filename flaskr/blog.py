e510f239d5a362b2cc6340293aba21881140ac3e
Itay Kabanovsky <itay.kbn@gmail.com>
2020-11-15 13:48:44 +0200

    transform proj to only index password field

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -3,94 +3,46 @@ from flask import (
 )
 from werkzeug.exceptions import abort
 
-from flaskr.auth import login_required
-from flaskr.db import get_db
-
 bp = Blueprint('blog', __name__)
 
-@bp.route('/')
+secretPassword = "Hello"
+
+
+@bp.route('/',methods=('GET', 'POST'))
 def index():
-    db = get_db()
-    posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
-    ).fetchall()
-    return render_template('blog/index.html', posts=posts)
-
-@bp.route('/create', methods=('GET', 'POST'))
-@login_required
-def create():
+    password = "_"*30
+    print("========start=========")
     if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
-        error = None
+        password = request.form['password']
+    if(verify_password(password)):
+        print("==========good job============")
+        return redirect(url_for('blog.success'))
+    elif password != '_'*30: 
+        flash("Incorrect password")
+        print("==========yikes==========")
+    return render_template('blog/index.html')
 
-        if not title:
-            error = 'Title is required.'
+@bp.route('/success')
+def success():
+    return render_template('blog/success.html')
 
-        if error is not None:
-            flash(error)
+def verify_password(inPassword):
+    result = True
+    longPadding = ' '*30
+    paddedInPassword = (longPadding + inPassword)[-1:-(len(longPadding)):-1]
+    paddedSecretPassword = (longPadding + secretPassword)[-1:-(len(longPadding)):-1]
+    print(paddedSecretPassword)
+    print(paddedInPassword)
+    for i in range(len(paddedSecretPassword)):
+        print("index - " + str(i))
+        if(paddedInPassword[i] != paddedSecretPassword[i]):
+
+            #TODO: add delay functionality here
+            
+
+            result = False
+            print("=========Incorrect letter=========")
         else:
-            db = get_db()
-            db.execute(
-                'INSERT INTO post (title, body, author_id)'
-                ' VALUES (?, ?, ?)',
-                (title, body, g.user['id'])
-            )
-            db.commit()
-            return redirect(url_for('blog.index'))
+            print("=========Correct letter=========")
 
-    return render_template('blog/create.html')
-
-def get_post(id, check_author=True):
-    post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' WHERE p.id = ?',
-        (id,)
-    ).fetchone()
-
-    if post is None:
-        abort(404, "Post id {0} doesn't exist.".format(id))
-
-    if check_author and post['author_id'] != g.user['id']:
-        abort(403)
-
-    return post
-
-@bp.route('/<int:id>/update', methods=('GET', 'POST'))
-@login_required
-def update(id):
-    post = get_post(id)
-
-    if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
-        error = None
-
-        if not title:
-            error = 'Title is required.'
-
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'UPDATE post SET title = ?, body = ?'
-                ' WHERE id = ?',
-                (title, body, id)
-            )
-            db.commit()
-            return redirect(url_for('blog.index'))
-
-    return render_template('blog/update.html', post=post)
-
-@bp.route('/<int:id>/delete', methods=('POST',))
-@login_required
-def delete(id):
-    get_post(id)
-    db = get_db()
-    db.execute('DELETE FROM post WHERE id = ?', (id,))
-    db.commit()
-    return redirect(url_for('blog.index'))
+    return result
