96afe2e37faa8e9f8f58dba36adee4d5b4fa9e21
Itay Kabanovsky <itay.kbn@gmail.com>
2020-11-15 21:54:39 +0200

    pass params through link

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -8,29 +8,31 @@ bp = Blueprint('blog', __name__)
 
 secretPassword = "Hello"
 
-
-@bp.route('/',methods=('GET', 'POST'))
-def index():
-    password = "_"*30
-    print("========start=========")
-    if request.method == 'POST':
-        password = request.form['password']
-    if( password != '_'*30):
-        if(verify_password(password)):
-            print("==========good job============")
-            return redirect(url_for('blog.success'))
-        else: 
-            flash("Incorrect password")
-            print("==========yikes==========")
+@bp.route('/')
+def main():
     return render_template('blog/index.html')
+
+
+@bp.route('/<string:password>/',methods=('GET', 'POST'))
+def index(password):
+    if(verify_password(password)):
+        print("==========good job============")
+        return redirect(url_for('blog.success'))
+    else: 
+        print("==========yikes==========")
+        return redirect(url_for('blog.failiure'))
 
 @bp.route('/success')
 def success():
     return render_template('blog/success.html')
 
+@bp.route('/failiure')
+def failiure():
+    return render_template('blog/failiure.html')
+
 def verify_password(inPassword):
     result = True
-    longPadding = ' '*30
+    longPadding = '_'*30
     paddedInPassword = (longPadding + inPassword)[-1:-(len(longPadding)):-1]
     paddedSecretPassword = (longPadding + secretPassword)[-1:-(len(longPadding)):-1]
     print(paddedSecretPassword)
