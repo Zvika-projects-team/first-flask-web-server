01250a1716e890661288f7408978e99943f54457
Itay Kabanovsky <itay.kbn@gmail.com>
2020-11-26 18:07:58 +0200

    blog thing

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -6,7 +6,7 @@ import time
 import random
 bp = Blueprint('blog', __name__)
 
-secretPassword = "Hello"
+secretPassword = "Pass"
 
 @bp.route('/')
 def main():
@@ -21,24 +21,55 @@ def auth(password=''):
         print("==========yikes==========")
         return b'0'
 
+
+delay  = 0.3
+next_delay = 0.3 
 def verify_password(inPassword):
-    result = True
-    longPadding = ' '*7
+    global delay
+    longPadding = ' '*6
     paddedInPassword = (longPadding + inPassword)[-(len(longPadding)):]
     paddedSecretPassword = (longPadding + secretPassword)[-(len(longPadding)):]
     print(paddedSecretPassword)
     print(paddedInPassword)
+    
+    return delay_basic(paddedInPassword,paddedSecretPassword)
 
-    delay = 0.15
+    #return delay_basic(paddedInPassword,paddedSecretPassword)
+
+delay = 0.15
+next_delay = 0.15
+def delay_geometric_seq(paddedInPassword, paddedSecretPassword):
+    global delay,next_delay
+    result = True
     for i in range(len(paddedSecretPassword)):
-        #print("index - " + str(i))
-        #print(f"{paddedInPassword[i]} != {paddedSecretPassword[i]}")
-        if(paddedInPassword[i] != paddedSecretPassword[i]):
-            #TODO: add delay functionality here 
-            result = False          
-            time.sleep(delay)
-            print(f"=========Incorrect letter=========")
-        else:
-            print("=========Correct letter=========")
+            #print("index - " + str(i))
+            #print(f"{paddedInPassword[i]} != {paddedSecretPassword[i]}")
+            if(paddedInPassword[i] != paddedSecretPassword[i]):
+                if result == True:
+                    next_delay = delay + 0.1
+                result = False
+                 
+                #TODO: add delay functionality here 
+                time.sleep(delay)
+                print(f"=========Incorrect letter=========")
+            else:
+                print("=========Correct letter=========")
+    delay = next_delay
+    return result
 
+
+def delay_basic(paddedInPassword, paddedSecretPassword):
+    global delay
+    result = True
+
+    for i in range(len(paddedSecretPassword)):
+            #print("index - " + str(i))
+            #print(f"{paddedInPassword[i]} != {paddedSecretPassword[i]}")
+            if(paddedInPassword[i] != paddedSecretPassword[i]):
+                result = False     
+                time.sleep(delay)
+                print(f"=========Incorrect letter=========")
+            else:
+                print("=========Correct letter=========")
+    delay = next_delay
     return result
